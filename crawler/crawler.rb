46494baa4fc232d31891a0f46608624f95edefe5
Eduardo Matos <eduardo.matos.silva@gmail.com>
2018-12-02 20:51:34 -0200

    Build crawler according to specs

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -1,14 +1,105 @@
+require 'uri'
 require 'http'
 require 'nokogiri'
 
 class Crawler
-  def get_url(url)
-    HTTP::get(url)
+  DEFAULT_MAX_REDIRECTS = 3
+  DEFAULT_TIMEOUT_IN_SECONDS = 5
+
+  def initialize(seed_url, timeout: DEFAULT_TIMEOUT_IN_SECONDS, max_redirects: DEFAULT_MAX_REDIRECTS)
+    @seed_uri = URI(seed_url)
+    @timeout = timeout
+    @max_redirects = max_redirects
+
+    validate_params(seed_url, timeout, max_redirects)
   end
 
-  def get_links(url)
-    response = get_url(url)
-    document = Nokogiri::parse(response)
-    document.css('a').map { |p| p['href'] }
+  def fetch_links(output_to: nil)
+    links = extract_links(fetch_html())
+
+    output(links, output_to) unless output_to.nil?
+
+    links
+  rescue HTTP::TimeoutError => e
+    raise RequestTimeoutError.new("Timed out after #{@timeout} seconds")
   end
+
+  def fetch_html
+    response = HTTP::follow(max_hops: @max_redirects).timeout(@timeout).get(@seed_uri.to_s)
+    raise_on_errors(response)
+    response.to_s
+  end
+
+  private
+
+  def validate_params(seed_url, timeout, max_redirects)
+    raise InvalidParam.new("Invalid URL: #{seed_url}") unless @seed_uri.kind_of?(URI::HTTP) || @seed_uri.kind_of?(URI::HTTPS)
+    raise InvalidParam.new("Invalid timeout: #{timeout}") unless timeout.is_a?(Integer)
+    raise InvalidParam.new("Invalid max redirects: #{max_redirects}") unless max_redirects.is_a?(Integer)
+  end
+
+  def extract_links(html)
+    hrefs = extract_hrefs(html)
+    hrefs = reject_external(hrefs)
+    hrefs = normalize_internal(hrefs)
+  end
+
+  def extract_hrefs(html)
+    document = Nokogiri::HTML(html)
+
+    links = document.css('a')
+
+    hrefs = links.map {|a| a['href']}
+    hrefs = hrefs.reject {|href| href.nil?}
+  end
+
+  def reject_external(hrefs)
+    hrefs.select do |href|
+      URI::parse(href).hostname == @seed_uri.hostname || href.start_with?('/')
+    end
+  end
+
+  def normalize_internal(hrefs)
+    hrefs.map do |href|
+      if href.start_with?('//')
+        next "#{@seed_uri.scheme}:#{href}"
+      elsif href.start_with?('/')
+        next base_url + href
+      end
+
+      href
+    end
+  end
+
+  def output(links, destination)
+    links.each {|link| destination.puts(link)}
+    destination.flush
+  end
+
+  def base_url
+    "#{@seed_uri.scheme}://#{@seed_uri.hostname}"
+  end
+
+  def raise_on_errors(response)
+    if response.status.code == 404
+      raise FetchError.new("Error fetching #{@seed_uri.to_s}")
+    end
+
+    if response.content_type.mime_type != 'text/html'
+      raise InvalidContentTypeError.new("Invalid content-type: #{response.content_type}")
+    end
+
+  end
+end
+
+class FetchError < Exception
+end
+
+class InvalidContentTypeError < Exception
+end
+
+class RequestTimeoutError < Exception
+end
+
+class InvalidParam < Exception
 end
--- crawler/spec/crawler_spec.rb
+++ crawler/spec/crawler_spec.rb
@@ -1,41 +1,104 @@
 require 'http'
+require 'stringio'
 require_relative '../crawler'
 
-describe 'Links' do
-  it 'returns a list of all links in a page' do
-    url = 'https://news.ycombinator.com'
-    crawler = Crawler.new
-    crawler.stub(:get_url).with(url).and_return('<a href="http://google.com"></a>')
-    links = ['http://google.com']
+describe Crawler do
+  let(:crawler) {
+    ->(
+      fixture_name = nil,
+      seed_url: 'http://fake.com/',
+      status: 200,
+      content_type: 'text/html',
+      timeout: 2,
+      max_redirects: 7
+    ) {
+      crawler = Crawler.new(seed_url, timeout: timeout, max_redirects: max_redirects)
 
-    crawler_links = crawler.get_links(url)
-    expect(crawler_links.class).to eq(Array)
-    expect(crawler_links).to eq(links)
-  end  
-end
+      resp = HTTP::Response.new({
+        version: 1.1,
+        status: status,
+        body: fixture_name.nil? ? '' : fixture(fixture_name),
+        headers: {content_type: content_type}
+      })
 
-describe 'inside links' do
-  it 'returns two links' do
-    url = 'https://news.ycombinator.com'
-    crawler = Crawler.new
-    crawler.stub(:get_url).with(url).and_return('<div><a href="http://google.com"></a><a href="http://facebook.com"></a></div>')
-    links = ['http://google.com', 'http://facebook.com']
-    
-    crawler_links = crawler.get_links(url)
-    expect(crawler_links.class).to eq(Array)
-    expect(crawler_links).to eq(links)
+      allow(HTTP).to receive(:follow).and_return(HTTP)
+      allow(HTTP).to receive(:timeout).and_return(HTTP)
+      allow(HTTP).to receive(:get).and_return(resp)
+
+      crawler
+    }
+  }
+
+  describe 'Response parsing' do
+    it 'returns links from page' do
+      expect(crawler['two-internal-links'].fetch_links).to eq(['http://fake.com/1', 'http://fake.com/2'])
+    end
+
+    it 'ignores external links' do
+      expect(crawler['one-internal-one-external'].fetch_links).to eq(['http://fake.com/3'])
+    end
+
+    it 'accepts internal links starting with slash' do
+      expect(crawler['relative-link'].fetch_links).to eq(['http://fake.com/4', 'http://fake.com/wow'])
+    end
+
+    it 'ignores empty hrefs' do
+      expect(crawler['empty-href'].fetch_links).to eq([])
+    end
+  end
+
+  describe 'Actual request' do
+    it 'makes request with correct params' do
+      crawler[max_redirects: 9, timeout: 8, seed_url: 'http://yay.wow'].fetch_links
+  
+      expect(HTTP).to have_received(:follow).with(max_hops: 9)
+      expect(HTTP).to have_received(:timeout).with(8)
+      expect(HTTP).to have_received(:get).with('http://yay.wow')
+    end
+  end
+
+  describe 'Output' do
+    it 'outputs found links' do
+      io = StringIO.new
+      crawler['two-internal-links'].fetch_links(output_to: io)
+  
+      expect(io.string).to eq("http://fake.com/1\nhttp://fake.com/2\n")
+    end
+  end
+
+  describe 'Errors' do
+    it 'raises error when request times out' do
+      url, timeout = 'http://fake.com', 19
+      crawler = Crawler.new(url, timeout: timeout)
+      allow(crawler).to receive(:fetch_html).and_raise(HTTP::TimeoutError)
+  
+      expect { crawler.fetch_links }.to raise_error(RequestTimeoutError, /Timed out after 19 seconds/)
+    end
+  
+    it 'raises exception when response status is 404' do
+      expect {crawler['empty-href', status: 404].fetch_links }.to raise_error(FetchError, "Error fetching http://fake.com/")
+    end
+  
+    it 'raises error when response is not HTML' do
+      expect {
+        crawler[content_type: 'application/json'].fetch_links
+      }.to raise_error(InvalidContentTypeError)
+    end
+  
+    it 'raises error when URL is invalid' do
+      expect { crawler[seed_url: 'wow.yay'].fetch_links }.to raise_error(InvalidParam, 'Invalid URL: wow.yay')
+    end
+  
+    it 'raises error when timeout is invalid' do
+      expect { crawler[timeout: 'spam'].fetch_links }.to raise_error(InvalidParam, 'Invalid timeout: spam')
+    end
+  
+    it 'raises error when timeout is invalid' do
+      expect { crawler[max_redirects: 'wow'].fetch_links }.to raise_error(InvalidParam, 'Invalid max redirects: wow')
+    end
   end
 end
 
-describe 'inside links' do
-  it 'returns two links' do
-    url = 'https://news.ycombinator.com'
-    crawler = Crawler.new
-    crawler.stub(:get_url).with(url).and_return('<div><a href="http://google.com"></a><a href="http://facebook.com"></a></div>')
-    links = ['http://google.com', 'http://facebook.com']
-    
-    crawler_links = crawler.get_links(url)
-    expect(crawler_links.class).to eq(Array)
-    expect(crawler_links).to eq(links)
-  end
+def fixture(name)
+  File.read(File.join(File.dirname(__FILE__), 'fixtures', "#{name}.html"))
 end
